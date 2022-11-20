166821e6ca24b9829c64e1f516c6c7bed9148586
Medeiros <hsymetry@gmail.com>
2022-11-19 22:38:11 -0300

    Basic ui to chatPage

--- lib/app/screens/home/home_page.dart
+++ lib/app/screens/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/app/widgets/custom_chat_bubble.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -35,96 +36,52 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
-          body: ListView(
+          body: Column(
             children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.all(10),
-                decoration: const BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Hi, friend!',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/7.png',
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return CustomChatBubble(
+                        align: index % 2 == 0
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        message: 'This is my first message!');
+                  },
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.all(10),
-                decoration: const BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Hi, friend!',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/7.png',
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 100,
-                decoration: const BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.add_circle_outline,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.send_outlined,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              )
             ],
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Container(
+              height: 60,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.only(
+                  // topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add_circle_outline,
+                      color: Colors.white,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.send_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
