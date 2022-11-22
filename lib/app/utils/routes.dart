55f9153f20edc72360376e828ec6763cdbcce5bb
Medeiros <hsymetry@gmail.com>
2022-11-21 23:27:19 -0300

    add namedRoutes

---
+++ lib/app/utils/routes.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+class NamedRoutes {
+
+  static const LOGIN_PAGE = '/';
+  static const CHAT_PAGE = '/chat-page';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
-import 'package:chat_app/app/screens/home/home_page.dart';
-import 'package:chat_app/app/screens/login/login_page.dart';
 import 'package:flutter/material.dart';
+
+import 'app/screens/home/chat_page.dart';
+import 'app/screens/login/login_page.dart';
+import 'app/utils/routes.dart';
 
 void main(List<String> args) {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          foregroundColor: Colors.black54,
+          actionsIconTheme: IconThemeData(
+            color: Colors.black54,
+          ),
+        ),
+      ),
       title: 'Chat app',
-      home: LoginPage(),
+      initialRoute: NamedRoutes.LOGIN_PAGE,
+      routes: {
+        NamedRoutes.LOGIN_PAGE: (_) => const LoginPage(),
+        NamedRoutes.CHAT_PAGE: (_) => const ChatPage(),
+      },
     ),
   );
 }
