4376bbdeca85428091ed9d814212f52d48a8c103
Medeiros <hsymetry@gmail.com>
2022-11-23 22:44:07 -0300

    Converting fromJson MessageEntity class

--- lib/app/models/chat_message_entity.dart
+++ lib/app/models/chat_message_entity.dart
@@ -1,24 +1,33 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ChatMessageEntity {
   String text;
   String? imageUrl;
-  int timeStamp;
   String id;
+  int createdAt;
   Author author;
 
-  ChatMessageEntity({
-    required this.text,
-    this.imageUrl,
-    required this.timeStamp,
-    required this.id,
-    required this.author,
-  });
+  ChatMessageEntity(
+      {required this.text,
+      required this.id,
+      required this.createdAt,
+      this.imageUrl,
+      required this.author});
+
+  factory ChatMessageEntity.fromJson(Map<String, dynamic> json) {
+    return ChatMessageEntity(
+      imageUrl: json['image'],
+        text: json['text'],
+        id: json['id'],
+        createdAt: json['createdAt'],
+        author: Author.fromJson(json['author']));
+  }
 }
 
 class Author {
   String username;
 
-  Author({
-    required this.username,
-  });
+  Author({required this.username});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(username: json['username']);
+  }
 }
