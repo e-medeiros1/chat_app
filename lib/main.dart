8c47acd3c67d218eb062ad3b940ca36fbb35d3fd
Medeiros <hsymetry@gmail.com>
2022-11-27 23:17:44 -0300

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'app/screens/home/chat_page.dart';
 import 'app/screens/login/login_page.dart';
@@ -6,26 +8,29 @@ import 'app/utils/routes.dart';
 
 void main(List<String> args) {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // fontFamily: 'Montserrat',
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          foregroundColor: Colors.black54,
-          actionsIconTheme: IconThemeData(
-            color: Colors.black54,
+    Provider(
+      create: (context) => AuthService(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          // fontFamily: 'Montserrat',
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            foregroundColor: Colors.black54,
+            actionsIconTheme: IconThemeData(
+              color: Colors.black54,
+            ),
           ),
         ),
+        title: 'Chat app',
+        // home: CustomGridviewGiphy(),
+        initialRoute: NamedRoutes.LOGIN_PAGE,
+        routes: {
+          NamedRoutes.LOGIN_PAGE: (_) => const LoginPage(),
+          NamedRoutes.CHAT_PAGE: (_) => const ChatPage(),
+        },
       ),
-      title: 'Chat app',
-      // home: CustomGridviewGiphy(),
-      initialRoute: NamedRoutes.LOGIN_PAGE,
-      routes: {
-        NamedRoutes.LOGIN_PAGE: (_) => const LoginPage(),
-        NamedRoutes.CHAT_PAGE: (_) => const ChatPage(),
-      },
     ),
   );
 }
