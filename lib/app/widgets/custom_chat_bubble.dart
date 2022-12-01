5cd37c4be62f2d8cd0a81f9de8947f6e87192515
Medeiros <hsymetry@gmail.com>
2022-11-30 22:31:00 -0300

    Add layout build to make web version looks better

--- lib/app/widgets/custom_chat_bubble.dart
+++ lib/app/widgets/custom_chat_bubble.dart
@@ -21,42 +21,48 @@ class CustomChatBubble extends StatelessWidget {
         entity.author.username == context.read<AuthService>().getUsername();
     return Align(
       alignment: align,
-      child: Container(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * .6,
-        ),
-        margin: const EdgeInsets.all(10),
-        padding: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          color: isAuthor ? Colors.deepPurple : Colors.black87,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(12),
-            topRight: Radius.circular(12),
-            bottomLeft: Radius.circular(12),
-          ),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(height: 10),
-            if (entity.imageUrl != null)
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    image:
-                        DecorationImage(image: NetworkImage(entity.imageUrl!)),
-                    borderRadius: BorderRadius.circular(12)),
-              ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                entity.text,
-                style: const TextStyle(color: Colors.white, fontSize: 20),
+      child: LayoutBuilder(
+        builder: (contexts, constraints) {
+          return Container(
+            constraints: BoxConstraints(
+              maxWidth: constraints.maxWidth < 800
+                  ? MediaQuery.of(context).size.width * .6
+                  : MediaQuery.of(context).size.width * .4,
+            ),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: isAuthor ? Colors.deepPurple : Colors.black87,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
               ),
             ),
-          ],
-        ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (entity.imageUrl != null)
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(entity.imageUrl!)),
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                if (entity.text.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      entity.text,
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
